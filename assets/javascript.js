3ae3e4912fb1bc552f32eb07f8e17f40704f72b1
xlmnope <lmgiroud@gmail.com>
2019-01-11 20:36:45 -0800

    gif plays and pauses on click

--- assets/javascript.js
+++ assets/javascript.js
@@ -35,7 +35,7 @@ function loadPage(){
     
   }
   $('.nav-tabs').append('<li class="nav-item"> <a class="nav-link nav-add" href="#"> +  </a>');
-  $('.nav-tabs').append('<form class="form-inline" style="display: none"> <input class="form-control mr-sm-2 form-control-sm" type="search" placeholder="Search" aria-label="Search"> <button class="btn btn-sm btn-outline-secondary my-2 my-sm-0" type="submit">Search</button> </form></li>');
+  $('.nav-tabs').append('<form class="form-inline" style="display: none"> <input class="form-control mr-sm-2 form-control-sm" type="search" placeholder="Search" id="search" aria-label="Search"> <button class="btn btn-sm btn-outline-secondary my-2 my-sm-0" id="search-button" type="submit">Search</button> </form></li>');
   
 }
 
@@ -52,8 +52,25 @@ $( ".nav-add" ).click(function() {
   $( ".form-inline" ).toggle();
 });
 
-
-
+$("#search-button").click(function() {
+  //save what customer searched 
+  var input = $("#search").val();
+  //add customer search to array
+  characters.push(input);
+  characterIndex = characters.last();
+  console.log(characterIndex);
+  $.ajax({
+    //use saved queryurl for character
+    url: characters[characterIndex].queryURL,
+    method: "GET"
+  }).then(function(response) {
+    //save response 
+    characters[characterIndex].giphyResponse = response
+    for (j = 0; j < 10; j++) {
+      $('.giphys').append('<img src="' + characters[characterIndex].giphyResponse.data[j].images.fixed_width_still.url + '" class="gif" data-state="still" id="'+ characters[characterIndex].name + j+'" >');
+  }
+  });
+});
 
 
 
@@ -66,28 +83,45 @@ $( ".nav-add" ).click(function() {
 function showGiphy(characterIndex){
   console.log(characterIndex);
   console.log(characters[characterIndex].queryURL)
-  if (characters[characterIndex].giphyResponse !== -1) {
+  //only make ajax call if giphyResponse doesn't already exist
+  if (!characters[characterIndex].giphyResponse.data) {
     $.ajax({
       //use saved queryurl for character
       url: characters[characterIndex].queryURL,
       method: "GET"
     }).then(function(response) {
+      //save response 
       characters[characterIndex].giphyResponse = response
       for (j = 0; j < 10; j++) {
-        $('.giphys').append('<img src="' + characters[characterIndex].giphyResponse.data[j].images.fixed_width_still.url + '" id="'+ characters[characterIndex].name + j+'" >');
-    }
+        $('.giphys').append('<img src="' + characters[characterIndex].giphyResponse.data[j].images.fixed_width_still.url + '" class="gif" data-state="still" data-img-id="'+j+'" data-character="'+characterIndex+'">');
+      }
+      bindGifs();
     });
-
- 
-
-}
+  }
 }
 
   
 
-$( ".gifstill" ).click(function() {
-  $( ".form-inline" ).toggle();
-});
+
+//on click of gif play gif
+function bindGifs(){
+  $(".gif").click(function() {
+    var state = $(this).attr("data-state");
+    var characterNumber = $(this).attr("data-character");
+      var imgNumber = $(this).attr("data-img-id");
+    if (state == 'still') {
+      var animatedUrl = characters[characterNumber].giphyResponse.data[imgNumber].images.fixed_width.url;
+      $(this).attr("src", animatedUrl);
+      $(this).attr("data-state", "animate");
+  }
+    if (state == 'animate') {
+    var stillURL = characters[characterNumber].giphyResponse.data[imgNumber].images.fixed_width_still.url
+    $(this).attr("src", stillURL);
+    state == 'animate'
+  }
+  });
+}
+
 
 function showMoreGifs(){
   // show ten gifs under the current ten giphs
